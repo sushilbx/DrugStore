704f3e07f4339a605b34a7ac066654c4ab685362
kaushal <kaushalbx@gmail.com>
2022-04-24 19:07:40 +0530

    Go one page to another page

--- app/src/main/java/com/ottego/drugstore/LoginActivity.java
+++ app/src/main/java/com/ottego/drugstore/LoginActivity.java
@@ -2,7 +2,11 @@ package com.ottego.drugstore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        Button next = (Button) findViewById(R.id.button);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
