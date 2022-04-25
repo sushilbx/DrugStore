2d1d0c6399b98073f46896a054c9585fd77c3845
kaushal <kaushalbx@gmail.com>
2022-04-26 00:42:50 +0530

    api in login

--- app/src/main/java/com/ottego/drugstore/LoginActivity.java
+++ app/src/main/java/com/ottego/drugstore/LoginActivity.java
@@ -1,28 +1,110 @@
 package com.ottego.drugstore;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.gson.Gson;
+import com.ottego.drugstore.model.SessionModel;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
+    TextInputEditText tietLoginEmail;
+    TextInputEditText tietLoginPassword;
+    MaterialButton mbLoginSubmit;
+    String url = Utils.URL + "admin_login";
+    String mobile = "";
+    String password = "";
+    SessionManager sessionManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        tietLoginEmail = findViewById(R.id.tietLoginEmail);
+        tietLoginPassword = findViewById(R.id.tietLoginPassword);
+        mbLoginSubmit = findViewById(R.id.mbLoginSubmit);
 
-        Button next = (Button) findViewById(R.id.button);
-        next.setOnClickListener(new View.OnClickListener() {
+        sessionManager = new SessionManager(LoginActivity.this);
+
+        mbLoginSubmit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                startActivity(intent);
+                mobile = tietLoginEmail.getText().toString().trim();
+                password = tietLoginPassword.getText().toString().trim();
+                login();
             }
 
         });
+
+
     }
+
+    void login() {
+        final ProgressDialog progressDialog = ProgressDialog.show(LoginActivity.this, null, "Processing...", false, false);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                progressDialog.dismiss();
+                Log.e("response", response);
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String code = jsonObject.getString("status");
+                    String message = jsonObject.getString("message");
+                    if (code.equalsIgnoreCase("true")) {
+                        JSONObject jsonObj = new JSONObject(jsonObject.getString("data"));
+                        Gson gson = new Gson();
+                        SessionModel sessionModel = gson.fromJson(String.valueOf(jsonObj), SessionModel.class);
+                        sessionManager.createLoginSession(sessionModel);
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+
+                    } else {
+                        Toast.makeText(LoginActivity.this, jsonObject.getString("message"), Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(LoginActivity.this, "Sorry, something went wrong.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.dismiss();
+                error.printStackTrace();
+                Toast.makeText(LoginActivity.this, " Please try again.", Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("mobile", mobile);
+                params.put("password", password);
+                return params;
+            }
+        };
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(30000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        MySingleton.myGetMySingleton(LoginActivity.this).myAddToRequest(stringRequest);
+    }
+
+
 }
---
+++ app/src/main/java/com/ottego/drugstore/Utils.java
@@ -0,0 +1,5 @@
+package com.ottego.drugstore;
+
+public class Utils {
+    public static final String URL = "http://ottego.com/api/store/";
+}
